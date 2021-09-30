ba90dea61ba860e2b911b739cc6ac1772e56bb89
3GamersStudios <80543119+3GamersStudios@users.noreply.github.com>
2021-09-29 21:29:53 -0700

    Added Beginning Files

---
+++ Chapter 2/Full_Name.py
@@ -0,0 +1,14 @@
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = f"{first_name} {last_name}"
+
+#or
+
+full_name = "{} {}".format(first_name,last_name)
+
+#print(full_name)
+
+message = f"Hello, {full_name.title()}!"
+
+print(message)
